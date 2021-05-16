23076462e383a366ac27f2ad19623cfc58437976
Gayan Kalinga <bg15407@gmail.com>
2021-05-16 11:06:04 +0530

    Initial Commit

---
+++ Word Scramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Word Scramble
+//
+//  Created by Gayan Kalinga on 5/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Gayan Kalinga on 5/16/21.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
