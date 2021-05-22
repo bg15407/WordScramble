7b0beae21e9764f3913d71cf69c8f8d7c00d44a2
Gayan Kalinga <bg15407@gmail.com>
2021-05-22 10:07:18 +0530

    final version with validations

--- Word Scramble/ContentView.swift
+++ Word Scramble/ContentView.swift
@@ -8,26 +8,111 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var newWord = ""
+    @State private var rootWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
     var body: some View {
-        List{
-            Section(header: Text("Start Static")){
-                Text("Hello, world!")
-                Text("Hello, world!")
-                Text("Hello, world!")
-            }
-            
-            Section(header: Text("Dynamic")){
-                ForEach(0..<6){number in
-                    Text("Dynamic \(number)")
+        NavigationView{
+            VStack{
+                TextField("Enter the word", text: $newWord, onCommit: addNewWord)
+                    .autocapitalization(.none)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                    
+                
+                List(usedWords, id: \.self){ word in
+                    Image(systemName: "\(word.count).circle")
+                    Text(word)
                 }
             }
-            
-            Section(header: Text("End Static")){
-                Text("Hello, end of the world!")
+            .navigationBarTitle(rootWord)
+            .alert(isPresented: $showingError) {
+                Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
             }
+            .onAppear(perform: startGame)
         }
     }
+    
+    func addNewWord(){
+        //Lowercase & trim
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        //Exit if answer is empty
+        guard answer.count > 0 else {
+            return
+        }
+        
+        //Extra Validation
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+
+        guard isReal(word: answer) else {
+            wordError(title: "Word not possible", message: "That isn't a real word.")
+            return
+        }
+        
+        usedWords.insert(answer, at: 0)
+        newWord = ""
+    }
+    
+    func startGame(){
+        if let fileURL = Bundle.main.url(forResource: "start", withExtension: "txt"){
+            if let startWord = try? String(contentsOf: fileURL){
+                let allWords = startWord.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "tree"
+                return
+            }
+        }
+        
+        fatalError("cannot load the file")
+    }
+    
+    //Same as Original word
+    func isOriginal(word: String) -> Bool{
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool{
+        var tempWord = rootWord
+        for letter in word{
+            if let pos = tempWord.firstIndex(of: letter){
+                tempWord.remove(at: pos)
+            }else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isReal(word: String) -> Bool{
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
